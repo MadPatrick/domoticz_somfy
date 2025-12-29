6577e7c433d8ed8d7a51779c558f26f9cba4e5d9
Patrick <81873830+MadPatrick@users.noreply.github.com>
2025-12-29 16:10:35 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -171,20 +171,16 @@ class BasePlugin:
                                 self.domoticz_host = value
                             elif key == "DOMOTICZ_PORT":
                                 self.domoticz_port = value
-                Domoticz.Log(f"config.txt succesvol gelezen: DOMOTICZ_HOST={self.domoticz_host}, DOMOTICZ_PORT={self.domoticz_port}")
-                logging.info(f"config.txt succesvol gelezen: DOMOTICZ_HOST={self.domoticz_host}, DOMOTICZ_PORT={self.domoticz_port}")
+                Domoticz.Log(f"config.txt loaded: DOMOTICZ_HOST={self.domoticz_host}, DOMOTICZ_PORT={self.domoticz_port}")
+                logging.info(f"config.txt loaded: DOMOTICZ_HOST={self.domoticz_host}, DOMOTICZ_PORT={self.domoticz_port}")
             except Exception as e:
-                Domoticz.Error(f"Fout bij lezen config.txt: {e}. Gebruik defaults: {self.domoticz_host}:{self.domoticz_port}")
-                logging.error(f"Fout bij lezen config.txt: {e}")
+                Domoticz.Error(f"Error with reading config.txt: {e}. Using defaults: {self.domoticz_host}:{self.domoticz_port}")
+                logging.error(f"Error with reading config.txt: {e}")
         else:
-            Domoticz.Status("config.txt niet gevonden in plugin-map. Gebruik defaults: 127.0.0.1:8080 voor sunrise/sunset API")
-            logging.warning("config.txt niet gevonden, gebruik defaults")
+            Domoticz.Status("config.txt not found in folder. Using defaults: 127.0.0.1:8080 for sunrise/sunset API")
+            logging.warning("config.txt  not found, Using defaults")
+        self.enabled = True
 
-        #check upgrading of version needs actions
-        self.version = Parameters["Version"]
-        self.enabled = self.checkVersion(self.version)
-        if not self.enabled:
-            return False
         pin = Parameters["Address"]
         port = int(Parameters["Port"])
        
@@ -446,11 +442,7 @@ class BasePlugin:
 
             if (dataset["deviceURL"].startswith("io://")):
                 dev = dataset["deviceURL"]
-                
-                # Use .get() to avoid KeyError
                 deviceClassTrig = dataset.get("deviceClass", "Unknown") 
-                
-                # Optioneel: Als deviceClass ontbreekt, probeer het uit de bestaande Domoticz device definitie te halen
                 if deviceClassTrig == "Unknown" and dev in Devices:
                      pass                
                 level = 0
@@ -529,35 +521,6 @@ class BasePlugin:
     def onDeviceRemoved(self, DeviceID, Unit):
         logging.debug("onDeviceRemoved called for DeviceID {0} and Unit {1}".format(DeviceID, Unit))
 
-    def checkVersion(self, version):
-        """checks actual version against stored version as 'Ma.Mi.Pa' and checks if updates needed"""
-        #read version from stored configuration
-        ConfVersion = getConfigItem("plugin version", "0.0.0")
-        Domoticz.Log("Starting version: " + version )
-        logging.info("Starting version: " + version )
-        MaCurrent,MiCurrent,PaCurrent = version.split('.')
-        MaConf,MiConf,PaConf = ConfVersion.split('.')
-        logging.debug("checking versions: current '{0}', config '{1}'".format(version, ConfVersion))
-        can_continue = True
-        if int(MaConf) < int(MaCurrent):
-            Domoticz.Log("Major version upgrade: {0} -> {1}".format(MaConf,MaCurrent))
-            logging.info("Major version upgrade: {0} -> {1}".format(MaConf,MaCurrent))
-            #add code to perform MAJOR upgrades
-            if int(MaConf) < 3:
-                can_continue = self.updateToEx()
-        elif int(MiConf) < int(MiCurrent):
-            Domoticz.Debug("Minor version upgrade: {0} -> {1}".format(MiConf,MiCurrent))
-            logging.debug("Minor version upgrade: {0} -> {1}".format(MiConf,MiCurrent))
-            #add code to perform MINOR upgrades
-        elif int(PaConf) < int(PaCurrent):
-            Domoticz.Debug("Patch version upgrade: {0} -> {1}".format(PaConf,PaCurrent))
-            logging.debug("Patch version upgrade: {0} -> {1}".format(PaConf,PaCurrent))
-            #add code to perform PATCH upgrades, if any
-        if ConfVersion != version and can_continue:
-            #store new version info
-            self._setVersion(MaCurrent,MiCurrent,PaCurrent)
-        return can_continue
-
     def create_devices(self, filtered_devices):
         logging.debug("create_devices: devices found, domoticz: "+str(len(Devices))+" API: "+str(len(filtered_devices)))
         created_devices = 0
@@ -634,14 +597,6 @@ class BasePlugin:
         else:
             return True
 
-    def _setVersion(self, major, minor, patch):
-        #set configs
-        logging.debug("Setting version to {0}.{1}.{2}".format(major, minor, patch))
-        setConfigItem(Key="MajorVersion", Value=major)
-        setConfigItem(Key="MinorVersion", Value=minor)
-        setConfigItem(Key="patchVersion", Value=patch)
-        setConfigItem(Key="plugin version", Value="{0}.{1}.{2}".format(major, minor, patch))
-       
 global _plugin
 _plugin = BasePlugin()
 

ab5f25867df5d4be1cf463c2a4d388399e2bc7db
Patrick <81873830+MadPatrick@users.noreply.github.com>
2026-01-29 21:18:46 +0100

    Revert "update table param"
    This reverts commit 3f87a9a96b3749bda48975e8982ec18386885dd3.

--- plugin.py
+++ plugin.py
@@ -6,10 +6,17 @@
 """
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="5.1.7" externallink="https://github.com/MadPatrick/somfy">
     <description>
         <br/><h2>Somfy Tahoma/Connexoon plugin</h2><br/>
         Version: 5.1.7
+=======
+<plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="5.1.5" externallink="https://github.com/MadPatrick/somfy">
+    <description>
+        <br/><h2>Somfy Tahoma/Connexoon plugin</h2><br/>
+        Version: 5.1.5
+>>>>>>> parent of b02400d (updated version)
 =======
 <plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="5.1.5" externallink="https://github.com/MadPatrick/somfy">
     <description>
